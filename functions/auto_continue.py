24c397bb5fb92221cb8de30c37643ab384974aa5
Adam Smith <adam@adamsmith.as>
2025-01-08 09:32:39 -0800

    use simpler, stateless auto-continue

--- functions/auto_continue.py
+++ functions/auto_continue.py
@@ -1,27 +1,20 @@
 import re
-from collections import defaultdict
-
-command_pattern = r"⟨[^⟩]+⟩"
 
 
 class Filter:
-    def __init__(self):
-        self.seen_commands_for_message = defaultdict(set)
+    def inlet(self, body):
+        body["stop"] = ["⟩"]
+        return body
 
-    async def outlet(self, body, __event_emitter__=None):
-        message = body["messages"][-1]
-        message_id = message["id"]
-        content = message["content"]
-
-        for command in re.findall(command_pattern, content):
-            if command in self.seen_commands_for_message[message_id]:
-                continue
-            self.seen_commands_for_message[message_id].add(command)
-            content = content.split(command)[0] + command
+    async def outlet(self, body, __event_emitter__):
+        content = body["messages"][-1]["content"]
+        if re.search(r"⟨[^⟩]*$", content):
             await __event_emitter__(
-                {"type": "replace", "data": {"content": content + "\n"}}
+                {
+                    "type": "message",
+                    "data": {"content": "⟩\n"},
+                }
             )
             await __event_emitter__({"type": "action", "data": {"action": "continue"}})
-            break
         else:
             return body
